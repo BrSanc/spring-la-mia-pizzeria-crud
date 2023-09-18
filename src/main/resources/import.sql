96547cc3a20f48f249f296b66954dfe668ed5bee
Sanchez <bradsanchez9845@gmail.com>
2023-09-18 09:35:54 +0200

    pizzeria 2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO pizza (name, description, price) VALUES('Margherita', 'Clasica Pizza', 9.99);
+INSERT INTO pizza (name, description, price) VALUES('Margherita', 'Clasica Pizza', 4.99);
+INSERT INTO pizza (name, description, price) VALUES('Diavola', 'Pizza con Salame Piccante', 6.99);
